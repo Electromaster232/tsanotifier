f3b406b90435c14e8957d7e7f399760642382e39
Electromaster232 <gilbert7573@gmail.com>
2022-04-21 09:42:47 -0400

    Notifier

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
-    FROMEMAIL = ""
+    FROMEMAIL = "PA-TSA Notifier <notify@theendlessweb.com>"
     UPLOAD_FOLDER = ''
     ALLOWED_EXTENSIONS = (['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-    DATABASE = ''
+    DATABASE = 'C:/Users/Adam Gilbert/PycharmProjects/notifier/newdata.db'
     OWNERKEY = ''
     SENDGRID = ""
-    SITEURL = ""
+    SITEURL = "http://127.0.0.1:5000/"
--- pypomf.py
+++ pypomf.py
@@ -5,18 +5,14 @@ import secrets
 from config import Config as Configvalues
 import random
 import time
-import sendgrid
-from sendgrid.helpers.mail import *
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = Configvalues.UPLOAD_FOLDER
 DATABASE = Configvalues.DATABASE
 random.seed()
 
-
-@app.route('/')
-def homepage():
-    return render_template('home.html')
 
 
 def get_db():
@@ -37,134 +33,73 @@ def allowed_file(filename):
     return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in Configvalues.ALLOWED_EXTENSIONS
 
-
-@app.route('/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-@app.route('/upload/<string:key>', methods=['GET', 'POST'])
-def upload_file(key):
-    # TODO: Refactor code
-    if request.method == 'POST':
-            # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            keylist = query_db("select * from key where allowed_keys = ? and ENABLED = \"YES\"", (key,))
-            if not keylist:
-                return '''<h2>ERROR: </h2> <p>Your token is not authorized to use this service.</p>'''
-            else:
-                extension = file.filename.split(".",1)[1]
-                randnum = random.getrandbits(20)
-                filename = str(randnum) + "." + extension
-                sqlname = filename.split(".",1)[0]
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                values = (key, str(time.time()), sqlname, extension)
-                query_db("INSERT INTO uploads (token, datetime, filename, fileext) VALUES (?, ?, ?, ?);", values)
-                db = getattr(g, '_database', None)
-                db.commit()
-                if not request.referrer:
-                    referrer = "/upload"
-                else:
-                    referrer = request.referrer
-                if "/panel/" in referrer:
-                    return redirect(referrer)
-                else:
-                    return redirect(url_for('uploaded_file',
-                                        filename=filename))
-    else:
-        return render_template('upload.html')
+@app.route('/process')
+def process():
+    req = requests.get("https://submissions.patsa.org/public/finalists.php")
+    soup = BeautifulSoup(req.text, "html.parser")
+    list = [tag for tag in soup.find_all("button")]
+    final = {}
+    processedlist = query_db("SELECT * FROM processed", ())
+    for i in list:
+        if i.get_text()[0:3] not in str(processedlist):
+            final[i.get_text()] = i.find_next_sibling("div")
+            query_db("INSERT INTO processed (eventid) VALUES (?)", (i.get_text()[0:3],))
+        else:
+            print("In processed list")
+    user_list = query_db("SELECT * FROM registrations WHERE enabled = \"yes\"")
+    for x in user_list:
+        for y,z in final.items():
+            from_email = Configvalues.FROMEMAIL
+            to_email = x[1]
+            subject = y
+            content = z
+            myobj = {'to': [to_email], 'from': from_email, 'subject': subject,
+                     'html_body': str(content)}
+            url = "https://postal.theendlessweb.com/api/v1/send/message"
+            headers = {'Content-Type': "application/json", 'X-Server-API-Key': "MH8hLnmN82jBm9jINCz8SpgQ",
+                       "Accept": '*/*'}
+            requests.post(url, json=myobj, headers=headers)
+    db = getattr(g, '_database', None)
+    db.commit()
+    return str(final)
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
     if request.method == "POST":
-        token = secrets.token_hex(20)
         verifkey = secrets.token_hex(10)
-        values = (token, request.form['email'], verifkey, "NO")
-        query_db("INSERT INTO key (allowed_keys, email, authtoken, ENABLED) VALUES (?, ?, ?, ?);", values)
+        values = (request.form['email'], "No", verifkey)
+        query_db("INSERT INTO registrations (email, enabled, authtoken) VALUES (?, ?, ?);", values)
         db = getattr(g, '_database', None)
         db.commit()
-        sg = sendgrid.SendGridAPIClient(apikey=Configvalues.SENDGRID)
-        from_email = Email(Configvalues.FROMEMAIL)
-        to_email = Email(request.form['email'])
-        subject = "PyPomf Email Verification"
-        content = Content("text/plain", "Hello from PyPomf! \n \nHere is your verification link!\n" + Configvalues.SITEURL + "/signup/verify/" + verifkey)
-        mail = Mail(from_email=from_email, subject=subject, to_email=to_email, content=content)
-        sg.client.mail.send.post(request_body=mail.get())
+        from_email = Configvalues.FROMEMAIL
+        to_email = request.form['email']
+        subject = "PA-TSA Notifier Email Verification"
+        content = "Hello from Notifier! \n \nHere is your verification link!\n" + Configvalues.SITEURL + "/signup/verify/" + verifkey
+        myobj = {'to': [to_email], 'from': from_email, 'subject': subject,
+                 'html_body': content}
+        url = "https://postal.theendlessweb.com/api/v1/send/message"
+        headers = {'Content-Type': "application/json", 'X-Server-API-Key': "MH8hLnmN82jBm9jINCz8SpgQ", "Accept": '*/*'}
+        res = requests.post(url, json=myobj, headers=headers)
+        if res.status_code != 200:
+            print(res)
         return '''<h2>Email sent sucessfully. Check your email for the verification code</h2>'''
 
 
 @app.route('/signup/verify/<string:veriftoken>')
 def verify(veriftoken):
-    validtoken = query_db("SELECT * FROM key WHERE authtoken = ?", (veriftoken,))
+    validtoken = query_db("SELECT * FROM registrations WHERE authtoken = ?", (veriftoken,))
     if not validtoken:
         return '''<h2>verification key unknown</h2>'''
     else:
-        query_db("UPDATE key SET ENABLED = \"YES\" WHERE authtoken = ?;", (veriftoken,))
+        query_db("UPDATE registrations SET enabled = \"yes\" WHERE authtoken = ?;", (veriftoken,))
         db = getattr(g, '_database', None)
         db.commit()
-        return render_template("signupcomplete.html", token=str(validtoken[0][0]))
+        return '''<h2>Success. You will now receive emails when events are posted</h2>'''
 
-
-@app.route('/upload')
-def uploadnokey():
-    return'''You need to supply your key to do this! Simply add it to the end of /upload/ for it to work!'''
-
-
-@app.route('/panel/<string:token>')
-def userpanel(token):
-    keylist = query_db("select * from key where allowed_keys = ? and ENABLED = \"YES\"", (token,))
-    if not keylist:
-        return '''<h2>Invalid Token</h2>'''
-    else:
-        filetotal = len(query_db("SELECT * from uploads"))
-        files = query_db("SELECT * from uploads WHERE token = ? ORDER BY id desc", (token,))
-        filelist = len(files)
-        file1 = None
-        file2 = None
-        file3 = None
-        file4 = None
-        file5 = None
-        try:
-            file1 = str(files[0][3]) + "." + str(files[0][4])
-            file2 = str(files[1][3]) + "." + str(files[1][4])
-            file3 = str(files[2][3]) + "." + str(files[2][4])
-            file4 = str(files[3][3]) + "." + str(files[3][4])
-            file5 = str(files[4][3]) + "." + str(files[3][4])
-        except IndexError:
-            pass
-        return render_template("panel.html", filenum=filelist, url="/../upload/" + token, file1=file1,
-                               file2=file2, file3=file3, file4=file4,
-                               file5=file5, totalfiles=filetotal)
-
-
-@app.route('/panel', methods=['GET', 'POST'])
-def panel():
-    if request.method == "GET":
-        return render_template("login.html")
-    if request.method == "POST":
-        url = "/panel/" + request.form['token']
-        return redirect(url)
-
-
-# Hack to fix users clicking on non-existant img links
-@app.route('/None')
-def none():
-    if request.referrer == None:
-        return redirect("/../")
-    else:
-        return redirect(request.referrer)
 
 
 if __name__ == '__main__':
